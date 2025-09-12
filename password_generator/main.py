ffdb0dee9c20235a8a89ba45c2b8c3e0f789b8b4
Reza <rmwhdy8@gmail.com>
2025-09-12 16:42:32 +0330

    main

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,8 +1,6 @@
 from utils import PasswordManager
 
-pas = PasswordManager()
 
-while True:
-    str_ = pas.string_password(12)
-    pas.save_password(str_)
-    pas.show_password(filename='password.txt')
+def main():
+    pass
+# creat main soon
